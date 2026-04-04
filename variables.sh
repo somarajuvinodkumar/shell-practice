fa301a6cd5831235880f7bee0d8895739383bab6
somaraju vinod <somarajuvinodkumar@gmail.com>
2026-04-04 15:57:11 +0530

    conversation.sh

---
+++ variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Modi
+PERSON2=Musk
+
+
+echo "$PERSON1:: hey $PERSON2, how are you?"
+echo "$PERSON2:: HELLO $PERSON1, i am fine. how are doing"
+echo "$PERSON1:: i am fine too .what'up?"
+echo "$PERSON1::nothing"
