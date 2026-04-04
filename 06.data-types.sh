cad08e98aa9a550b22f81e1745978f28df36378f
somaraju vinod <somarajuvinodkumar@gmail.com>
2026-04-04 16:18:36 +0530

    data-types.sh

--- 06.data-types.sh
+++ 06.data-types.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+TIMESTAMP=$(year)
 
 
 NUMBER1=100
@@ -9,4 +9,3 @@ NUMBER2=200
 echo "Script executed at: $TIMESTAMP"
 SUM=$(($NUMBER1+$NUMBER2))
 
-echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
