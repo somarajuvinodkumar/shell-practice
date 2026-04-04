f11a7385d88004984a3cfa74548db6d78df6811f
somaraju vinod <somarajuvinodkumar@gmail.com>
2026-04-04 16:26:15 +0530

    data-types.sh

---
+++ arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("Court" "HIT3" "PUSHPA2" "RRR")
+
+#echo "First Movie: ${MOVIES[0]}"
+#echo "last Movie: ${MOVIES[3]}"
+echo "First Movie: ${MOVIES[4]}"
+
+#echo "All movies: ${MOVIES[@]}"
